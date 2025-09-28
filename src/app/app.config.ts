c6782359036ab76a8df795979b99c802fd93cfc5
antoniopetruzzella <a.petruzzella71@gmail.com>
2025-09-28 16:31:23 +0200

    primo commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { firebaseConfig } from '../environments/firebase-config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter([]), // aggiungerai le rotte qui
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../environments/firebase-config';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('firestoredbmanager');
+   constructor() {
+    const app = initializeApp(firebaseConfig);
+    console.log('Firebase initialized:', app.name);
+    const auth = getAuth();
+signInWithEmailAndPassword(auth, 'xxx', 'xxx')
+  .then((result) => {
+    console.log('✅ Login riuscito:', result.user.email);
+  })
+  .catch((error) => {
+    console.error('❌ Errore login:', error.code, error.message);
+  });
+  }
 }
